590f7639c81e3b8712af6319aeb6c0737e8690e6
JMiclat404 <miclativan123@gmail.com>
2025-05-06 02:30:27 +0800

    Verify OTP added

--- database/dbconnection.php
+++ database/dbconnection.php
@@ -1,28 +1,23 @@
 <?php
-
     class Database
     {
-        
         private $host;
         private $db_name;
+        private $port;
         private $username;
         private $password;
-        
         public $conn;
 
-        
         public function __construct()
         {
-            
-            if($_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_NAME'] === '192.168.1.72'){
-                
+            if($_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_ADDR'] === '127.0.0.1'){
                 $this->host = "localhost";
                 $this->db_name = "itelec2";
+                $this->port = "3307";
                 $this->username = "root";
                 $this->password = "";
             }
             else{
-                
                 $this->host = "localhost";
                 $this->db_name = "";
                 $this->username = "";
@@ -30,22 +25,16 @@
             }
         }
 
-        
         public function dbConnection()
         {
-            
             $this->conn = null;
             try {
-                
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-                
+                $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $exception) 
             {
-                
                 echo "Connection error: " . $exception->getMessage();
             }
-            
             return $this->conn;
         }
     }
